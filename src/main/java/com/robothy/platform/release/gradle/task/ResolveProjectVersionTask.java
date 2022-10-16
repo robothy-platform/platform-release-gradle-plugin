5496d3ee9858b77b249ee67d0f1fa1070a74be86
Robothy Luo <robothyluo@gmail.com>
2022-10-16 16:54:06 +0800

    fix task circulary dependency

--- src/main/java/com/robothy/platform/release/gradle/task/ResolveProjectVersionTask.java
+++ src/main/java/com/robothy/platform/release/gradle/task/ResolveProjectVersionTask.java
@@ -4,14 +4,23 @@ import java.util.Objects;
 import java.util.Optional;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.Project;
+import org.gradle.api.logging.Logging;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskContainer;
+import org.slf4j.Logger;
 
 /**
  * Resolve the project version. If the release option is true, then
  * remove the '-SNAPSHOT' suffix from {@code project.version}.
  */
 public class ResolveProjectVersionTask extends DefaultTask {
+
+  private static final Logger log = Logging.getLogger(ResolveProjectVersionTask.class);
+
+  public ResolveProjectVersionTask() {
+    Project project = getProject();
+    project.afterEvaluate(this::configureDependentTask);
+  }
 
   @TaskAction
   public void execute() {
@@ -20,9 +29,8 @@ public class ResolveProjectVersionTask extends DefaultTask {
     if (Objects.equals("true", release)) {
       String version = project.getVersion().toString();
       project.setVersion(version.substring(0, version.lastIndexOf('-')));
+      log.debug("Resolved project version is '{}'", project.getVersion());
     }
-
-    project.afterEvaluate(this::configureDependentTask);
   }
 
   /**
@@ -31,7 +39,11 @@ public class ResolveProjectVersionTask extends DefaultTask {
   private void configureDependentTask(Project project) {
     TaskContainer tasks = project.getTasks();
     Optional<ResolveProjectVersionTask> resolveProjectVersionTask = tasks.withType(ResolveProjectVersionTask.class).stream().findFirst();
-    resolveProjectVersionTask.ifPresent(projectVersionTask -> tasks.configureEach(task -> task.dependsOn(projectVersionTask)));
+    resolveProjectVersionTask.ifPresent(projectVersionTask -> tasks.configureEach(task -> {
+      if (!(task instanceof ResolveProjectVersionTask)) {
+        task.dependsOn(projectVersionTask);
+      }
+    }));
   }
 
 }
--- src/test/java/com/robothy/platform/release/gradle/task/ResolveProjectVersionTaskTest.java
+++ src/test/java/com/robothy/platform/release/gradle/task/ResolveProjectVersionTaskTest.java
@@ -1,4 +1,42 @@
+package com.robothy.platform.release.gradle.task;
+
 import static org.junit.jupiter.api.Assertions.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.gradle.testkit.runner.BuildResult;
+import org.gradle.testkit.runner.GradleRunner;
+import org.gradle.testkit.runner.TaskOutcome;
+import org.junit.jupiter.api.Test;
+
 class ResolveProjectVersionTaskTest {
-  
+
+  @Test
+  public void test() throws IOException {
+    String resolveProjectVersion = "resolveProjectVersion";
+
+    Path projectPath = Files.createTempDirectory("gradle-project");
+    File buildFile = new File(projectPath.toFile(), "build.gradle");
+    File settingsFile = new File(projectPath.toFile(), "settings.gradle");
+    Files.writeString(buildFile.toPath(), """
+        plugins {
+          id 'robothy-platform-release'
+        }
+        """);
+    Files.writeString(settingsFile.toPath(), """
+        rootProject.name = 'test-resolve-project-version'
+        """);
+    projectPath.toFile().deleteOnExit();
+    buildFile.deleteOnExit();
+    settingsFile.deleteOnExit();
+
+    BuildResult result = GradleRunner.create()
+        .withProjectDir(projectPath.toFile())
+        .withArguments(resolveProjectVersion)
+        .withPluginClasspath()
+        .build();
+    assertSame(result.task(":" + resolveProjectVersion).getOutcome(), TaskOutcome.SUCCESS);
+  }
+
 }
